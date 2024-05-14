6b56f3666b9d3e15d264fcf231c6624f51315b62
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2024-05-13 22:35:09 -0400

    chapter4 is finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter/counter.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonsComponent
   ],
   imports: [
     BrowserModule
