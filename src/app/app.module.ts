b5f87722c7a164cc232df64268d6c937663c4860
JeongAhYoon <131531559+JeongAhYoon@users.noreply.github.com>
2024-05-24 12:16:43 -0400

    chapter 26 is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { AppRoutingModule } from './app-routing-modules';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppReducer } from './store/app.state';
-
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule} from '@angular/common/http'
 
 
 @NgModule({
@@ -15,16 +16,16 @@ import { AppReducer } from './store/app.state';
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    
-    
   ],
   imports: [
     BrowserModule, 
     StoreModule.forRoot( {} ),
     AppRoutingModule,
+    HttpClientModule,
     StoreDevtoolsModule.instrument({
       logOnly: !isDevMode(), // Restrict extension to log-only mode
     }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,6 +7,8 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { StoreModule } from "@ngrx/store";
 import { AUTH_STATE_NAME } from "./state/auth.selector";
 import { AuthReducer } from "./state/auth.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { AuthEffects } from "./state/auth.effects";
 
 
 const routes:Routes = [
@@ -24,6 +26,7 @@ const routes:Routes = [
     imports:[
         CommonModule,
         ReactiveFormsModule,
+        EffectsModule.forFeature([AuthEffects]),
         RouterModule.forChild(routes),
         StoreModule.forFeature(AUTH_STATE_NAME, AuthReducer),
     ]
